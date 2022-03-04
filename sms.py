38b20c1260e107e4df8d9850cb611f682a46a394
raphael-khan <raphaelkhan@yahoo.com>
2022-03-03 21:56:55 -0500

    3.3.22

--- sms.py
+++ sms.py
@@ -7,7 +7,7 @@ client = Client(account_sid, auth_token)
 message = client.messages.create(  
                               messaging_service_sid='MGae2fdb196c2c961a03974f1c759f72ff', 
                               body='Hi, I can believe this works ',      
-                              to=''  #  number you want to text. 
+                              to=''  #  number you want to text / sms.
                           )
 message.sid
 print('message you sent successfully !!')
