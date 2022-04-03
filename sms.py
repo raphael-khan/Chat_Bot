e736538cc28d7e0c1330f737899fbdc6f28242b5
raphael-khan <raphaelkhan@yahoo.com>
2022-04-02 23:03:49 -0400

    updates

--- sms.py
+++ sms.py
@@ -1,13 +1,14 @@
 from twilio.rest import Client 
 
-account_sid = '' # account sid from twilio
-auth_token = '' # auth token from twilio 
-client = Client(account_sid, auth_token) 
+account_sid = 'ACf8c1f39d0febe3e02c6f384afae51a10' # account sid from twilio
+auth_token = '164f5edc118443f46df57c17fac9a7b5' # auth token from twilio 
+client = Client(account_sid, auth_token)
 
 message = client.messages.create(  
                               messaging_service_sid='MGae2fdb196c2c961a03974f1c759f72ff', 
-                              body='Hi, I can believe this works ',      
-                              to=''  #  number you want to text / sms.....
+                              body='Just look at this and you will know ! '  
+                              'https://media.giphy.com/media/126OMdMaXFWLL2/giphy.gif',      
+                              to=''  #  number you want to text / sms......
                           )
 message.sid
 print('message you sent successfully !!')
